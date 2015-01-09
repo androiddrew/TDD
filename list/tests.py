e85dee27c73ac2605d15e23bc9c9048dc5696a5d
Drew Bednar <Drewbednar@Drews-MacBook-Pro-2.local>
2015-01-08 22:42:53 -0500

    Refactor home page view to use a template

--- list/tests.py
+++ list/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from list.views import home_page
 
@@ -20,8 +21,13 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		#decode is used to convert the response.content bytes into a unicode string
+		self.assertEqual(response.content.decode(), expected_html)
 
-		
+		#just remember that the response.content is going to be in bytes which is why we use b before
+		#the string
+		# self.assertTrue(response.content.startswith(b'<html>'))
+		# self.assertIn(b'<title>To-Do lists</title>', response.content)
+		# self.assertTrue(response.content.endswith(b'</html>'))
+
