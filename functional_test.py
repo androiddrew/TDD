3dfa5c2da771403990157cc9265049cf73ace36a
Drew Bednar <Drewbednar@Drews-MacBook-Pro-2.local>
2015-01-10 16:26:17 -0500

    Refactoring the functional tests to inspect the HTML template produced by view

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+#import time
 
 class NewVisitorTest(unittest.TestCase):
 	# We are overriding the TestCase objects Setup and Teardowns which bookend the program when
@@ -10,6 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
+		#time.sleep(100)
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
@@ -40,14 +42,24 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		#We can add our own failure message as an argument to most assert methods in the unittest module
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table")
-		
+		#Turns out the generator is a little too clever
+		#self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+		#	"New to-do item did not appear in table")
+		#Much simpler test because selenium returns an iterable list when we use the elements function
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 		self.fail('Finish the test!')
- 		# There is still a text box inviting her to add another item. She
-		 # enters "Use peacock feathers to make a fly" (Edith is very methodical)
- 
- 		# The page updates again, and now shows both items on her list
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
