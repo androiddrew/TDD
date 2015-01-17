9a0510e48b4d1d4b8a31efdbb3cde7dcfb315b1a
Drew Bednar <Drewbednar@Drews-MacBook-Pro-2.local>
2015-01-16 22:24:52 -0500

    Set STATIC_ROOT in settings and disabled admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -83,3 +83,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+#Need to add the root setting to collect all our static files. 
+#These will be served by the webserve in a diff directory
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
