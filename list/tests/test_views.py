c68ab5cccde406014c9b4770c6cb5204c6cd8d34
Drew Bednar <Drewbednar@Drews-MacBook-Pro-2.local>
2015-01-18 20:23:56 -0500

    Refactor list view to handle new item POSTs

--- list/tests/test_views.py
+++ list/tests/test_views.py
@@ -12,26 +12,6 @@ from list.models import Item, List
 
 class NewListTest(TestCase):
 
-
-	def test_can_save_a_POST_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-
-		self.client.post('/lists/%d/add_item' %(correct_list.id,), data={'item_text':'A new item for an existing list'})
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new item for an existing list')
-		self.assertEqual(new_item.list, correct_list)
-
-	def test_redirects_to_list_view(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
-
-		response = self.client.post('/lists/%d/add_item' %(correct_list.id,), data={'item_text':'A new item for an existing list'})
-
-		self.assertRedirects(response, '/lists/%d/' %(correct_list.id))
-
 	def test_saving_a_POST_request(self):
 		# commenting this out because we could use the test client provided by django instead
 		# request = HttpRequest()
@@ -158,3 +138,25 @@ class ListViewTest(TestCase):
 		correct_list = List.objects.create()
 		response = self.client.get('/lists/%d/' % (correct_list.id))
 		self.assertEqual(response.context['list'], correct_list)
+
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post('/lists/%d/' % (correct_list.id), data={"item_text": "A new item for an existing list"})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, "A new item for an existing list")
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_POST_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		
+		response = self.client.post(
+				'/lists/%d/' % (correct_list.id,),
+				data={'item_text': 'A new item for an existing list'}
+			)
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
--- list/urls.py
+++ list/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^(\d+)/$', 'list.views.view_list', name='view_list'),
-    url(r'^(\d+)/add_item$', 'list.views.add_item', name='add_item'),
     url(r'^new$', 'list.views.new_list', name='new_list'),
 )
